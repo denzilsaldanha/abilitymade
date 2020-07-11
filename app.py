6662d924651294fdda351cd853864f6f4230786a
denzil <denzilsaldanha@gmail.com>
2020-07-11 11:45:50 +1000

    Added index.html and script.js Written code for Get Orthotists

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
+
 
 class Orthotist:
     def __init__(self, **Orthotist):
@@ -10,27 +11,43 @@ class Orthotist:
         self.practicePh = Orthotist['practicePh']
         self.orthotistName = Orthotist['orthotistName']
 
-#List of Orthotists
-orthotistsList = [Orthotist(**{'practiceName':'P1',
-                             'practiceAddress':'Kensington',
-                             'practicePh':'0401254789',
-                             'orthotistName':'o1'}),
+
+# List of Orthotists
+orthotistsList = [Orthotist(**{'practiceName': 'P1',
+                               'practiceAddress': 'Kensington',
+                               'practicePh': '0401254789',
+                               'orthotistName': 'o1'}),
                   Orthotist(**{'practiceName': 'P2',
                                'practiceAddress': 'Mascot',
                                'practicePh': '0408795478',
-                               'orthotistName': 'o2'},),
-                  Orthotist(**{'practiceName':'P3',
-                             'practiceAddress':'Randwick',
-                             'practicePh':'0401254129',
-                             'orthotistName':'o3'}),
-                  Orthotist(**{'practiceName':'P4',
-                             'practiceAddress':'Botany',
-                             'practicePh':'0408987789',
-                             'orthotistName':'o4'}),
-                  Orthotist(**{'practiceName':'P5',
-                             'practiceAddress':'Glenfield',
-                             'practicePh':'0401212389',
-                             'orthotistName':'o5'})
+                               'orthotistName': 'o2'}, ),
+                  Orthotist(**{'practiceName': 'P3',
+                               'practiceAddress': 'Randwick',
+                               'practicePh': '0401254129',
+                               'orthotistName': 'o3'}),
+                  Orthotist(**{'practiceName': 'P4',
+                               'practiceAddress': 'Botany',
+                               'practicePh': '0408987789',
+                               'orthotistName': 'o4'}),
+                  Orthotist(**{'practiceName': 'P5',
+                               'practiceAddress': 'Glenfield',
+                               'practicePh': '0401212389',
+                               'orthotistName': 'o5'})
                   ]
-for obj in orthotistsList:
-    print(obj.__dict__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/orthotist', methods =["GET"])
+def orthotist():
+    if request.method == 'GET':
+        orthotistJson = []
+        for obj in orthotistsList:
+            orthotistJson.append(obj.__dict__)
+        return json.dumps(orthotistJson)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
